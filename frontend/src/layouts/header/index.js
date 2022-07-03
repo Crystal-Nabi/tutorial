5ef7b3506725173eec497feb4652e986044bb380
paullong <paullong0505.gmail.com>
2022-07-03 18:15:27 +0900

    update header

--- frontend/src/layouts/header/index.js
+++ frontend/src/layouts/header/index.js
@@ -1,22 +1,46 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Box from "@mui/material/Box";
+import "./navbar.scss";
 const PageHeader = () => {
   return (
-    <header>
-      <Box
-        display="flex"
-        justifyContent={"space-between"}
-        style={{ marginLeft: "3rem", marginRight: "3rem" }}
-      >
-        <Box>
-          <NavLink to="/">Home</NavLink>
-        </Box>
-        <Box>
-          <NavLink to="/signin">signin</NavLink>
-          <NavLink to="/signup">signup</NavLink>
-        </Box>
-      </Box>
+    <header className="navbar">
+      <div>
+        <ul className="menu">
+          <li>
+            <NavLink to="/">HOME</NavLink>
+          </li>
+          <li>
+            <NavLink to="/introduce">INTRODUCE</NavLink>
+            <ul className="submenu">
+              <li>
+                <NavLink to="/introduce/our">OUR</NavLink>
+              </li>
+              <li>
+                <NavLink to="/introduce/me"> ME</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <NavLink to="/about">ABOUT</NavLink>
+            <ul className="submenu">
+              <li>
+                <NavLink to="/about/our">about team</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about/me"> about SCG</NavLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className="menu">
+          <li>
+            <NavLink to="/login">login</NavLink>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
